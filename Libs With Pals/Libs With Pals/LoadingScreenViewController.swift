5245d6619c218619c829e1422e7886518ea3360d
Rethi <jlr5452@lib-pclem025.lib.utexas.edu>
2018-03-30 19:46:10 -0500

    adding loading screen

---
+++ Libs With Pals/Libs With Pals/LoadingScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingScreenViewController.swift
+//  Libs With Pals
+//
+//  Created by Rethi, Jennifer L on 3/30/18.
+//  Copyright © 2018 Group8. All rights reserved.
+//
+
+import UIKit
+
+class LoadingScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+}
