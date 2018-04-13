1cdc3222b9af22122795251e3a6db1a00f1d4bab
Gerlou Shyy <gerloushyy@Gerlous-MacBook-Air.local>
2018-04-13 02:19:54 -0500

    user can draw picturesand clear the screen if they want to erase the drawing

--- Libs With Pals/Libs With Pals/DrawingViewController.swift
+++ Libs With Pals/Libs With Pals/DrawingViewController.swift
@@ -10,17 +10,10 @@ import UIKit
 
 class DrawingViewController: UIViewController {
     
-    
-    @IBOutlet weak var mainImageView: UIImageView!
-    @IBOutlet weak var tempImageView: UIImageView!
-    
-    var lastPoint = CGPoint(x:0, y:0)
-    var red: CGFloat = 0.0
-    var green: CGFloat = 0.0
-    var blue: CGFloat = 0.0
-    var brushWidth: CGFloat = 10.0
-    var opacity: CGFloat = 1.0
-    var swiped = false
+    @IBOutlet weak var canvasView: UIView!
+    var path = UIBezierPath()
+    var startPoint = CGPoint()
+    var touchPoint = CGPoint()
     
     @IBAction func onButtonPressed(_ sender: Any) {
         self.performSegue(withIdentifier: "drawSegue", sender: AnyClass.self)
@@ -36,74 +29,43 @@ class DrawingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        swiped = false
-        if let touch = touches.first as? UITouch {
-            lastPoint = touch.location(in: self.view)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let point = touch?.location(in: canvasView) {
+            startPoint = point
         }
     }
     
-    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
-        
-        // 1
-        UIGraphicsBeginImageContext(view.frame.size)
-        let context = UIGraphicsGetCurrentContext()
-        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
-        
-        // 2
-        let current = CGPoint(x:fromPoint.x, y:fromPoint.y)
-        context?.move(to: current)
-        //CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
-        //CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
-        context?.addLine(to: current)
-        
-        // 3
-        /*
-        CGContextSetLineCap(context, kCGLineCapRound)
-        CGContextSetLineWidth(context, brushWidth)
-        CGContextSetRGBStrokeColor(context, red, green, blue, 1.0)
-        CGContextSetBlendMode(context, kCGBlendModeNormal)
- */
-        
-        // 4
-        context!.strokePath()
-        
-        // 5
-        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        tempImageView.alpha = opacity
-        UIGraphicsEndImageContext()
-        
-    }
-    
-    func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
-        // 6
-        swiped = true
-        if let touch = touches.first as? UITouch {
-            let currentPoint = touch.location(in: view)
-            drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint)
-            
-            // 7
-            lastPoint = currentPoint
-        }
-    }
-    
-    func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
-        
-        if !swiped {
-            // draw a single point
-            drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint)
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let point = touch?.location(in: canvasView) {
+            touchPoint = point
         }
         
-        /*
-        // Merge tempImageView into mainImageView
-        UIGraphicsBeginImageContext(mainImageView.frame.size)
-        mainImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: kCGBlendModeNormal, alpha: 1.0)
-        tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: kCGBlendModeNormal, alpha: opacity)
-        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        */
-        tempImageView.image = nil
+        // draw a smooth line
+        path.move(to: startPoint)
+        path.addLine(to: touchPoint)
+        startPoint = touchPoint
+        
+        // call draw
+        draw()
     }
     
-    
+    // let users draw a picture
+    func draw() {
+        let strokeLayer = CAShapeLayer()
+        strokeLayer.fillColor = nil
+        strokeLayer.lineWidth = 5
+        strokeLayer.strokeColor = UIColor.black.cgColor
+        strokeLayer.path = path.cgPath
+        canvasView.layer.addSublayer(strokeLayer)
+        canvasView.layer.setNeedsDisplay()
+    }
+
+    // clear the screen
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        path.removeAllPoints()
+        canvasView.layer.sublayers = nil
+        canvasView.setNeedsDisplay()
+    }
 }
