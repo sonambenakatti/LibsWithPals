73e00a504388f0a03a1d921c8b73d7e0a6e7d849
sonambenakatti <sonam.benakatti@gmail.com>
2018-04-16 23:39:45 -0500

    more work on instruction page

--- Libs With Pals/Libs With Pals/HowToPlayViewController.swift
+++ Libs With Pals/Libs With Pals/HowToPlayViewController.swift
@@ -14,19 +14,19 @@ class HowToPlayViewController: UIViewController {
     @IBOutlet weak var howToPlayImage: UIImageView!
     
     @IBOutlet weak var instructionsText: UITextView!
-    let text = ["1. Choose an option from the storyline library",
-                "2. Choose words according to the given type",
-                "2. Choose words according to the given type",
-                "3. View your completed storyline!"]
+    let text = ["Choose an option from the storyline library",
+                "Choose words according to the given type",
+                "Choose words according to the given type",
+                "View your completed storyline!"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        instructionsText.centerVertically()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        instructionsText.textAlignment = .center
-        
+
         let screenSize: CGRect = UIScreen.main.bounds
         self.hand.frame = CGRect(x: screenSize.width / 1.8, y: screenSize.height / 1.4, width: self.hand.frame.width / 2, height: self.hand.frame.height / 2)
         self.view.addSubview(self.hand)
@@ -45,7 +45,7 @@ class HowToPlayViewController: UIViewController {
             withDuration: 3.0,
             animations: {
                 self.hand.center.x -= 50;
-                self.hand.center.y -= 30;
+                self.hand.center.y -= 45;
             }
         )
     }
@@ -92,15 +92,25 @@ class HowToPlayViewController: UIViewController {
                 self.instructionsText.alpha = 0.0;
         }, completion: { _ in
             if(self.hand.isHidden == false) {
-                print("resetting hand values")
                 self.hand.isHidden = true
                 let screenSize: CGRect = UIScreen.main.bounds
-                self.hand.center.x = screenSize.width / 1.8
-                self.hand.center.y = screenSize.height / 1.4
+                self.hand.frame.origin.x = screenSize.width / 1.8
+                self.hand.frame.origin.y = screenSize.height / 1.4
             }
             self.fadeTextIn(val: val + 1)
         })
     }
-
-        
 }
+
+extension UITextView {
+    
+    func centerVertically() {
+        let fittingSize = CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude)
+        let size = sizeThatFits(fittingSize)
+        let topOffset = (bounds.size.height - size.height * zoomScale) / 2
+        let positiveTopOffset = max(1, topOffset)
+        contentOffset.y = -positiveTopOffset
+    }
+    
+}
+
